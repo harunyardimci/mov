e23702c46a364525ee16c3dc72db7eeebbeaf594
Harun yardimci <harun.yardimci@gmail.com>
2012-07-26 11:53:39 +0300

    -

--- index.php
+++ index.php
@@ -1,35 +1,52 @@
 <DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr" lang="tr">
-	<head>
-	<!-- page title -->
-	<title></title>
+    <head>
+        <!-- page title -->
+        <title></title>
 
-	<!-- meta data -->
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta http-equiv="Content-Language" content="en"/>
-	<meta name="description" content=""/>
-	<meta name="keywords" content="movie, artist, cast, genre"/>
-	<meta name="rating" content="general"/>
-	<meta http-equiv="Pragma" content="no-cache"/>
-	<meta http-equiv="Expires" content="0"/>
-	<meta http-equiv="Cache-Control" content="max-age=0"/>
-	<meta name="robots" content="NOODP,index,follow"/>
-	<meta name="revisit-after" content="1 Days"/>
-	
-	<link rel="canonical" href="http://www.mmm.com/"/>
-	<link rel="stylesheet" type="text/css" media="screen" href="/static/css/global.css"/>
+        <!-- meta data -->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Content-Language" content="en"/>
+        <meta name="description" content=""/>
+        <meta name="keywords" content="movie, artist, cast, genre"/>
+        <meta name="rating" content="general"/>
+        <meta http-equiv="Pragma" content="no-cache"/>
+        <meta http-equiv="Expires" content="0"/>
+        <meta http-equiv="Cache-Control" content="max-age=0"/>
+        <meta name="robots" content="NOODP,index,follow"/>
+        <meta name="revisit-after" content="1 Days"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="harun.yardimci@gmail.com">
+
+        <!-- styles -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="assets/css/docs.css" rel="stylesheet">
+        <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+        <!-- IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- fav and touch icons -->
+        <link rel="shortcut icon" href="assets/img/favicon.ico">
+
+        <link rel="canonical" href="http://www.mmm.com/"/>
 	
 	</head>
 
 	<body>
-		<h1>Movie Search</h1>
-		<form  class="form-horizontal well">
-	  	<div class="control-group">
-            		<div class="controls">
-              			<input type="text" class="input-xxlarge search-query" value="Movie name, title, cast">&nbsp;<a class="btn btn-info btn-large" href="#">Search</a>
-              			<p class="help-block">In addition to freeform text, any HTML5 text-based input appears like so.</p>
-            		</div>
-          	</div>
-		</form>
+        <div class="container">
+            <h1>Movie Search</h1>
+            <form>
+                <div class="row">
+                        <div class="control-group">
+                            <input type="text" class=input-xxlarge search-query" value="Movie name, title, cast">&nbsp;<a class="btn btn-info btn-large" href="#">Search</a>
+                            <p class="help-block">In addition to freeform text, any HTML5 text-based input appears like so.</p>
+                        </div>
+                </div>
+            </form>
+        </div>
 	</body>
 </html>
