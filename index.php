9f62fa2c39851fc5560559c517a9d598e3b8718a
harun yardimci <hyardimci@harun.(none)>
2012-07-26 08:15:03 +0300

    -

---
+++ index.php
@@ -0,0 +1,35 @@
+<DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr" lang="tr">
+	<head>
+	<!-- page title -->
+	<title></title>
+
+	<!-- meta data -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta http-equiv="Content-Language" content="en"/>
+	<meta name="description" content=""/>
+	<meta name="keywords" content="movie, artist, cast, genre"/>
+	<meta name="rating" content="general"/>
+	<meta http-equiv="Pragma" content="no-cache"/>
+	<meta http-equiv="Expires" content="0"/>
+	<meta http-equiv="Cache-Control" content="max-age=0"/>
+	<meta name="robots" content="NOODP,index,follow"/>
+	<meta name="revisit-after" content="1 Days"/>
+	
+	<link rel="canonical" href="http://www.mmm.com/"/>
+	<link rel="stylesheet" type="text/css" media="screen" href="/static/css/global.css"/>
+	
+	</head>
+
+	<body>
+		<h1>Movie Search</h1>
+		<form  class="form-horizontal well">
+	  	<div class="control-group">
+            		<div class="controls">
+              			<input type="text" class="input-xxlarge search-query" value="Movie name, title, cast">&nbsp;<a class="btn btn-info btn-large" href="#">Search</a>
+              			<p class="help-block">In addition to freeform text, any HTML5 text-based input appears like so.</p>
+            		</div>
+          	</div>
+		</form>
+	</body>
+</html>
